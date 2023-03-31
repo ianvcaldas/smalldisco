974a2613d6cda78defae52fd5d4a92a66b9859ce
yahmed <yahmed@its-rc-yahmed.ad.syr.edu>
2023-03-31 14:45:50 -0400

    added quick installation test process

--- example/disco_test.sh
+++ example/disco_test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 ## Test script for smalldisco vignette
+
+python ../smalldisco.py sirna -o sirna.bed -a sample.gtf BAM/
+
+python ../smalldisco.py tail -o tails.tsv -g genome.fa --tailor_command ../Tailor/bin/tailor_v1.1_linux_static sirna.bed BAM/
